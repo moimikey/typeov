bbbc9da87231f1456595e6d13112b2991b98b8a2
Michael Scott Hertzberg <mshertzberg@gmail.com>
2016-03-18 13:52:47 -0400

    first release

---
+++ src/index.js
@@ -0,0 +1,29 @@
+export function typeov(input) {
+  const isArray = Array.isArray
+  const isNaN = Number.isNaN
+  const isSafeInteger = Number.isSafeInteger
+  const isFinite = Number.isFinite
+  switch(typeof input) {
+    case 'string':
+      return 'string'
+    case 'object':
+      if (input === null) return 'null'
+      if (isArray(input)) return 'array'
+      return 'object'
+    case 'number':
+      if (isNaN(input)) return 'nan'
+      if (isSafeInteger(input)) return 'number'
+      if (isFinite(input)) return 'float'
+      return 'infinity'
+    case 'undefined':
+      return 'undefined'
+    case 'function':
+      return 'function'
+    case 'boolean':
+      return 'boolean'
+    case 'symbol':
+      return 'symbol'
+    default:
+      return 'unsure?'
+  }
+}
---
+++ src/index.spec.js
@@ -0,0 +1,51 @@
+import * as tap from 'tap'
+import { typeov } from './'
+const test = tap.test
+test('float', t => {
+  t.equal(typeov(Math.PI), 'float')
+  t.end()
+})
+test('number', t => {
+  t.equal(typeov(666), 'number')
+  t.equal(typeov(+0), 'number')
+  t.equal(typeov(-0), 'number')
+  t.end()
+})
+test('string', t => {
+  t.equal(typeov('hello world'), 'string')
+  t.end()
+})
+test('boolean', t => {
+  t.equal(typeov(true), 'boolean')
+  t.end()
+})
+test('symbol', t => {
+  t.equal(typeov(Symbol()), 'symbol')
+  t.end()
+})
+test('null', t => {
+  t.equal(typeov(null), 'null')
+  t.end()
+})
+test('infinity', t => {
+  t.equal(typeov(Infinity), 'infinity')
+  t.equal(typeov(-Infinity), 'infinity')
+  t.equal(typeov(+Infinity), 'infinity')
+  t.end()
+})
+test('undefined', t => {
+  t.equal(typeov(undefined), 'undefined')
+  t.end()
+})
+test('function', t => {
+  t.equal(typeov(function(){}), 'function')
+  t.end()
+})
+test('object', t => {
+  t.equal(typeov({}), 'object')
+  t.end()
+})
+test('array', t => {
+  t.equal(typeov([]), 'array')
+  t.end()
+})
